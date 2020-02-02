73f2a407a2a8e4407e7a0a38add9349711e3259b
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2020-02-02 07:25:39 +0000

    [finish]5-5

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show]
 
-  resources :post_images, only: [:new, :create, :index, :show] do
-      resource :favorites, only: [:create, :destroy]
-      resource :post_comments, only: [:create, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show]
 end
