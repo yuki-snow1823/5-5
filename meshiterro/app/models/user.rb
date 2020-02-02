3ffe864bfb9d93278bf676602735440b4299b40f
yuki-snow1823 <magic.iroha@gmail.com>
2020-02-02 15:40:06 +0900

    [start]5-5

---
+++ meshiterro/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+end
+
+# 5-4:User(Nではない方)に関連させたいモデルを書くということかも
+# favariteはいいね機能のやつ
